6122c8beece4d8789b2b201a6e503772aac91a91
vishalyadav-77 <vishalyadav15044@gmail.com>
2024-09-15 20:30:33 +0530

    New_items.xml and Adapter added

---
+++ app/src/main/java/com/example/taazakhabar/RecAdapter.java
@@ -0,0 +1,64 @@
+package com.example.taazakhabar;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class RecAdapter extends RecyclerView.Adapter<RecAdapter.NewsViewHolder> {
+    private List<Article> articles;
+    private Context context;
+    public RecAdapter(Context context, List<Article> articles) {
+        this.articles = articles;
+        this.context=context;
+    }
+
+    @NonNull
+    @Override
+    public NewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_items, parent, false);
+        return new NewsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsViewHolder holder, int position) {
+        Article article = articles.get(position);
+        holder.titleTextView.setText(article.getTitle());
+        holder.descriptionTextView.setText(article.getDescription());
+        // Load image with Glide or Picasso
+        Glide.with(context).load(article.getUrlToImage()).into(holder.imageThumbnail);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return articles.size();
+    }
+
+    public void updateNews(List<Article> articles) {
+        this.articles = articles;
+        notifyDataSetChanged();
+    }
+
+    public static class NewsViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTextView;
+        TextView descriptionTextView;
+        ImageView imageThumbnail;
+
+        public NewsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.titleTextView);
+            descriptionTextView = itemView.findViewById(R.id.descriptionTextView);
+            imageThumbnail = itemView.findViewById(R.id.thumbnailImageView);
+        }
+    }
+}
