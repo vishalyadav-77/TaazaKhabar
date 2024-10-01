614c3902d689142691ef363de90e394d9c428813
vishalyadav-77 <vishalyadav15044@gmail.com>
2024-10-01 17:32:38 +0530

    webview implemented

--- app/src/main/java/com/example/taazakhabar/News_Details.java
+++ app/src/main/java/com/example/taazakhabar/News_Details.java
@@ -1,11 +1,10 @@
 package com.example.taazakhabar;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.bumptech.glide.Glide;
-
 public class News_Details extends AppCompatActivity {
-    TextView titleview, contentview, readbutton;
-    ImageView thumnailview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,31 +24,15 @@ public class News_Details extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        titleview = findViewById(R.id.title2);
-        contentview = findViewById(R.id.content);
-        thumnailview = findViewById(R.id.imageView);
-        readbutton = findViewById(R.id.readmore);
+        WebView webView = findViewById(R.id.webView);
 
         Intent intent = getIntent();
-        String title = intent.getStringExtra("title");
-        String thumbnail = intent.getStringExtra("thumbnail");
-        String content = intent.getStringExtra("content");
         String url= intent.getStringExtra("url");
 
-        if (content.contains("[+")) {
-            // Handle truncated content
-            readbutton.setVisibility(View.VISIBLE);
-            readbutton.setOnClickListener(view -> {
-                // Open full article in a WebView or browser
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(browserIntent);
-            });
-        } else {
-            readbutton.setVisibility(View.GONE);
-        }
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
-        titleview.setText(title);
-        contentview.setText(content);
-        Glide.with(this).load(thumbnail).into(thumnailview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
     }
 }
